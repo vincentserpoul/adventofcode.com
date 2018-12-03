48bc687b29b0e18c2f6f4c3f8df0bf90414303d9
Vincent Serpoul <vincent@serpoul.com>
2018-12-03 18:08:33 +0800

    day 1

--- 2018/day/1/src/main.rs
+++ 2018/day/1/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+
+fn main() -> io::Result<()> {
+    println!("last freq: {}", part1());
+    println!("first repeated freq: {}", part2());
+    Ok(())
+}
+
+fn part1() -> i32 {
+    let f = File::open("input.txt").unwrap();
+    let reader = BufReader::new(f);
+    BufReader::new(reader)
+        .lines()
+        .fold(0i32, |sum, l| sum + l.unwrap().parse::<i32>().unwrap())
+}
+
+fn part2() -> i32 {
+    let mut freqs = HashSet::new();
+    let mut curr_freq = 0;
+
+    loop {
+        let f = File::open("input.txt").unwrap();
+        let reader = BufReader::new(f);
+        for l in BufReader::new(reader).lines() {
+            curr_freq += l.unwrap().parse::<i32>().unwrap();
+            if freqs.contains(&curr_freq) {
+                return curr_freq;
+            }
+            freqs.insert(curr_freq);
+        }
+    }
 }
