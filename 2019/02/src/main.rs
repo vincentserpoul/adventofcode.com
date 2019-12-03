27c19b0fa24052ff659392bac275612c5ca94af5
Vincent Serpoul <vincent@serpoul.com>
2019-12-03 09:54:33 +0800

    day 2 2/2

--- 2019/02/src/main.rs
+++ 2019/02/src/main.rs
@@ -8,46 +8,56 @@ const ADD: i32 = 1;
 const MUL: i32 = 2;
 const END: i32 = 99;
 
+const WANTED: i32 = 19_690_720;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut it = input
-        .split(',')
-        .map(|code| code.parse().unwrap())
-        .collect::<Vec<i32>>();
+    'noun: for noun in 0..99 {
+        for verb in 0..99 {
+            let mut it = input
+                .split(',')
+                .map(|code| code.parse().unwrap())
+                .collect::<Vec<i32>>();
 
-    let mut cur_idx: usize = 0;
-    loop {
-        match it[cur_idx] {
-            END => {
-                println!("found 99");
-                break;
-            }
-            ADD => {
-                let left_idx = it[cur_idx + 1] as usize;
-                let right_idx = it[cur_idx + 2] as usize;
-                let res_idx = it[cur_idx + 3] as usize;
-                println!("updating {} with {}", res_idx, it[left_idx] + it[right_idx]);
-                it[res_idx] = it[left_idx] + it[right_idx];
-                cur_idx += 4;
-            }
-            MUL => {
-                let left_idx = it[cur_idx + 1] as usize;
-                let right_idx = it[cur_idx + 2] as usize;
-                let res_idx = it[cur_idx + 3] as usize;
-                println!("updating {} with {}", res_idx, it[left_idx] * it[right_idx]);
-                it[res_idx] = it[left_idx] * it[right_idx];
-                cur_idx += 4;
-            }
-            _ => {
-                println!("_ found {}", it[cur_idx]);
-                break;
+            // let noun = 12;
+            // let verb = 2;
+
+            it[1] = noun;
+            it[2] = verb;
+
+            let mut cur_idx: usize = 0;
+            'calc: loop {
+                match it[cur_idx] {
+                    END => {
+                        if it[0] == WANTED {
+                            println!("{}", noun * 100 + verb);
+                            break 'noun;
+                        }
+                        break 'calc;
+                    }
+                    ADD => {
+                        let left_idx = it[cur_idx + 1] as usize;
+                        let right_idx = it[cur_idx + 2] as usize;
+                        let res_idx = it[cur_idx + 3] as usize;
+                        it[res_idx] = it[left_idx] + it[right_idx];
+                        cur_idx += 4;
+                    }
+                    MUL => {
+                        let left_idx = it[cur_idx + 1] as usize;
+                        let right_idx = it[cur_idx + 2] as usize;
+                        let res_idx = it[cur_idx + 3] as usize;
+                        it[res_idx] = it[left_idx] * it[right_idx];
+                        cur_idx += 4;
+                    }
+                    _ => {
+                        break 'calc;
+                    }
+                }
             }
         }
     }
-
-    println!("{:?}", it[0]);
 
     Ok(())
 }
